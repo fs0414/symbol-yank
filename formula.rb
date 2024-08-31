7ea158173b0785f16635b77601bf6969aad5e784
fujitani sora <fujitanisora0414@gmail.com>
2024-08-31 22:42:12 +0900

    formula fix

--- formula.rb
+++ formula.rb
@@ -1,7 +1,7 @@
 class SymbolicYank < Formula
-  desc "ランダムな動物の絵文字をクリップボードにコピーするCLIツール"
-  homepage "https://github.com/fs0414/homebrew-symbol-yank"
-  url "https://github.com/fs0414/homebrew-symbol-yank/archive/v1.0.0.tar.gz"
+  desc ""
+  homepage "https://github.com/fs0414/symbol-yank"
+  url "https://github.com/fs0414/symbol-yank/archive/v1.0.0.tar.gz"
   sha256 "c588e412349ada6a728ef9b6140a83f3738de7cbe78dff4ad813c9f30245b952"
   license "MIT"
 
@@ -12,6 +12,6 @@ class SymbolicYank < Formula
   end
 
   test do
-    system "#{bin}/homebrew-symbol-yank"
+    system "#{bin}/symbol-yank"
   end
 end
